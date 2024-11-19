1a006bdf550bf1a2471fabae2709c883a93e24ef
Dmitriy Sofronov <profos24@gmail.com>
2024-11-20 01:47:08 +0400

    test: Refactor tests for test_1

--- test_1/test_solution.py
+++ test_1/test_solution.py
@@ -1,43 +1,39 @@
-import unittest
+import pytest
 from solution import strict
 
-class TestStrictDecorator(unittest.TestCase):
-    def test_correct_types(self):
-        @strict
-        def test_func(a: int, b: str) -> bool:
-            return len(b) > a
+def test_correct_types():
+    @strict
+    def test_func(a: int, b: str) -> bool:
+        return len(b) > a
 
-        self.assertTrue(test_func(2, "hello"))
+    assert test_func(2, "hello")
 
-    def test_incorrect_int_type(self):
-        @strict
-        def test_func(a: int, b: str) -> bool:
-            return len(b) > a
+def test_incorrect_int_type():
+    @strict
+    def test_func(a: int, b: str) -> bool:
+        return len(b) > a
 
-        with self.assertRaises(TypeError):
-            test_func("2", "hello")
+    with pytest.raises(TypeError):
+        test_func("2", "hello")
 
-    def test_incorrect_str_type(self):
-        @strict
-        def test_func(a: int, b: str) -> bool:
-            return len(b) > a
+def test_incorrect_str_type():
+    @strict
+    def test_func(a: int, b: str) -> bool:
+        return len(b) > a
 
-        with self.assertRaises(TypeError):
-            test_func(2, 5)
+    with pytest.raises(TypeError):
+        test_func(2, 5)
 
-    def test_float_and_bool(self):
-        @strict
-        def test_func(a: float, b: bool) -> float:
-            return a * (1 if b else -1)
+def test_float_and_bool():
+    @strict
+    def test_func(a: float, b: bool) -> float:
+        return a * (1 if b else -1)
 
-        self.assertEqual(test_func(3.14, True), 3.14)
-        self.assertEqual(test_func(3.14, False), -3.14)
+    assert test_func(3.14, True) == 3.14
+    assert test_func(3.14, False) == -3.14
 
-        with self.assertRaises(TypeError):
-            test_func("3.14", True)
+    with pytest.raises(TypeError):
+        test_func("3.14", True)
 
-        with self.assertRaises(TypeError):
-            test_func(3.14, 1)
-
-if __name__ == '__main__':
-    unittest.main()
+    with pytest.raises(TypeError):
+        test_func(3.14, 1)
